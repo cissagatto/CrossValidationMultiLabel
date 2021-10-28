ec451ab3d425f0d4cbd324d645eb112cc87070d0
Elaine Cecília Gatto <elainececiliagatto@gmail.com>
2021-10-28 20:22:04 -0300

    Update cvm.R

--- R/cvm.R
+++ R/cvm.R
@@ -114,77 +114,7 @@ cat("\n nome \t ", dataset_name)
 
 ##################################################################################################
 # cat("\nCopy FROM google drive \n")
-
-##################################################################################################
-# Validation
-##################################################################################################
-validation <- as.numeric(args[4])
-cat("\n validation \t ", validation)
-
-
-##################################################################################################
-# folder results                                                                                 #
-##################################################################################################
-FolderResults <- toString(args[5])
-cat("\n folder \t ", FolderResults)
-
-
-##################################################################################################
-# Get dataset name                                                                               #
-##################################################################################################
-dataset_name <- toString(ds$Name) 
-cat("\n nome \t ", dataset_name)
-
-
-##################################################################################################
-# DON'T RUN -- it's only for test the code
-# ds <- datasets[29,]
-# dataset_name = ds$Name
-# number_dataset = ds$Id
-# number_cores = 10
-# number_folds = 10
-# FolderResults = "/dev/shm/res"
-# validation = 1
-##################################################################################################
-
-
-##################################################################################################
 # destino = paste(FolderRoot, "/Datasets/Originais/", dataset_name, sep="")
-
-##################################################################################################
-# Validation
-##################################################################################################
-validation <- as.numeric(args[4])
-cat("\n validation \t ", validation)
-
-
-##################################################################################################
-# folder results                                                                                 #
-##################################################################################################
-FolderResults <- toString(args[5])
-cat("\n folder \t ", FolderResults)
-
-
-##################################################################################################
-# Get dataset name                                                                               #
-##################################################################################################
-dataset_name <- toString(ds$Name) 
-cat("\n nome \t ", dataset_name)
-
-
-##################################################################################################
-# DON'T RUN -- it's only for test the code
-# ds <- datasets[29,]
-# dataset_name = ds$Name
-# number_dataset = ds$Id
-# number_cores = 10
-# number_folds = 10
-# FolderResults = "/dev/shm/res"
-# validation = 1
-##################################################################################################
-
-
-##################################################################################################
 # origem = paste("cloud:elaine/Datasets/Originais/", dataset_name, ".arff", sep="")
 # comando = paste("rclone -v copy ", origem, " ", destino, sep="")
 # cat("\n", comando, "\n") 
@@ -195,11 +125,11 @@ cat("\n nome \t ", dataset_name)
 #   quit("yes")
 # }
 
-# destino = paste(FolderRoot, "/datasets/", dataset_name, sep="")
+# destino = paste(FolderRoot, "/Datasets/Originais/", dataset_name, sep="")
 # origem = paste("cloud:elaine/Datasets/Originais/", dataset_name, ".xml", sep="")
 # comando = paste("rclone -v copy ", origem, " ", destino, sep="")
 # cat("\n", comando, "\n") 
-# a  = print(system(comando))
+# a = print(system(comando))
 # a = as.numeric(a)
 # if(a != 0) {
 #   stop("Erro RCLONE")
@@ -226,7 +156,7 @@ folders = createDirs(FolderResults)
 # CREATING FOLDERS                                                                               #
 ##################################################################################################  
 cat("\nCross Validation")
-timeCVM = system.time(res <- CrossValidationMultiLabel(folders, ds, dataset_name, number_cores,
+timeCVM = system.time(res <- CrossValidationMultiLabel(folders, ds, dataset_name, 
                                                        number_dataset, number_folds, 
                                                        validation, folderResults))
 cat("\n")
